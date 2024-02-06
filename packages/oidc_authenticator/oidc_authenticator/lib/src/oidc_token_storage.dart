7a303881e7bf58e5d2f9173ef26b676a38c0fd6f
daniellampl <dnl.lampl@gmail.com>
2024-02-06 15:35:24 -0500

    fix: adjust OidcTokenStorage to storage changes

--- packages/oidc_authenticator/oidc_authenticator/lib/src/oidc_token_storage.dart
+++ packages/oidc_authenticator/oidc_authenticator/lib/src/oidc_token_storage.dart
@@ -1,3 +1,4 @@
+import 'package:authenticator/authenticator.dart';
 import 'package:authenticator_storage/authenticator_storage.dart';
 import 'package:oidc_authenticator_platform_interface/oidc_authenticator_platform_interface.dart';
 
@@ -7,40 +8,66 @@ const _hiveTokenTypeKey = 'AUTHENTICATOR_TOKEN_TYPE';
 const _hiveRefreshTokenKey = 'AUTHENTICATOR_REFRESH_TOKEN';
 const _hiveIdTokenKey = 'AUTHENTICATOR_ID_TOKEN';
 
-class OidcTokenStorage extends SecureAuthenticatorStorage<OidcToken> {
+class OidcTokenStorage extends AuthenticatorStorage<OidcToken> {
+  OidcTokenStorage({
+    SecureAuthenticatorStorage? storage,
+  }) : _storage = storage ?? SecureAuthenticatorStorage();
+
+  final SecureAuthenticatorStorage _storage;
+
+  @override
+  Future<void> initialize() async {
+    // this method should become obsolte once enough users migrated.
+    try {
+      await _storage.migrate([
+        _hiveAccessTokenKey,
+        _hiveAccessTokenExpirationKey,
+        _hiveTokenTypeKey,
+        _hiveRefreshTokenKey,
+        _hiveIdTokenKey,
+      ]);
+    } catch (e) {
+      // ignore errors, we just don't migrate.
+    }
+  }
+
   @override
   Future<void> persistToken(OidcToken token) async {
     await Future.wait([
-      writeTokenvalue(_hiveAccessTokenKey, token.accessToken),
-      writeTokenvalue(
-        _hiveAccessTokenExpirationKey,
-        token.expiresIn,
-      ),
-      writeTokenvalue(_hiveTokenTypeKey, token.tokenType),
-      writeTokenvalue(_hiveRefreshTokenKey, token.refreshToken),
-      writeTokenvalue(_hiveIdTokenKey, token.idToken),
+      _storage.writeTokenvalue(_hiveAccessTokenKey, token.accessToken),
+      if (token.expiresIn != null)
+        _storage.writeTokenvalue(
+          _hiveAccessTokenExpirationKey,
+          token.expiresIn!.toString(),
+        ),
+      if (token.tokenType != null)
+        _storage.writeTokenvalue(_hiveTokenTypeKey, token.tokenType!),
+      if (token.refreshToken != null)
+        _storage.writeTokenvalue(_hiveRefreshTokenKey, token.refreshToken!),
+      if (token.idToken != null)
+        _storage.writeTokenvalue(_hiveIdTokenKey, token.idToken!),
     ]);
   }
 
   @override
   Future<void> removePersistedToken() async {
     await Future.wait([
-      deleteTokenValue(_hiveAccessTokenKey),
-      deleteTokenValue(_hiveAccessTokenExpirationKey),
-      deleteTokenValue(_hiveTokenTypeKey),
-      deleteTokenValue(_hiveRefreshTokenKey),
-      deleteTokenValue(_hiveIdTokenKey),
+      _storage.deleteTokenValue(_hiveAccessTokenKey),
+      _storage.deleteTokenValue(_hiveAccessTokenExpirationKey),
+      _storage.deleteTokenValue(_hiveTokenTypeKey),
+      _storage.deleteTokenValue(_hiveRefreshTokenKey),
+      _storage.deleteTokenValue(_hiveIdTokenKey),
     ]);
   }
 
   @override
   Future<OidcToken?> get token async {
     final tokenValues = await Future.wait<dynamic>([
-      readTokenValue(_hiveAccessTokenKey),
-      readTokenValue(_hiveAccessTokenExpirationKey),
-      readTokenValue(_hiveTokenTypeKey),
-      readTokenValue(_hiveRefreshTokenKey),
-      readTokenValue(_hiveIdTokenKey),
+      _storage.readTokenValue(_hiveAccessTokenKey),
+      _storage.readTokenValue(_hiveAccessTokenExpirationKey),
+      _storage.readTokenValue(_hiveTokenTypeKey),
+      _storage.readTokenValue(_hiveRefreshTokenKey),
+      _storage.readTokenValue(_hiveIdTokenKey),
     ]);
 
     final accessToken = tokenValues[0] as String?;
@@ -50,7 +77,7 @@ class OidcTokenStorage extends SecureAuthenticatorStorage<OidcToken> {
 
     return OidcToken(
       accessToken: accessToken,
-      expiresIn: tokenValues[1] as int?,
+      expiresIn: tokenValues[1] != null ? int.parse(tokenValues[1]) : null,
       tokenType: tokenValues[2] as String?,
       refreshToken: tokenValues[3] as String?,
       idToken: tokenValues[4] as String?,
